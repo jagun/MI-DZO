6d8d3e2d3d20948c89e2bd27c55d5e55a262867c
Sergii Stamenov <sstamenov@profinit.eu>
2016-05-16 14:48:09 +0200

    script finished, readme

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# CTU MI-DZO 2015/2016
+# Texture Synthesis by Non-parametric Sampling
+# python implementation of [article by Alexei A. Efros and Thomas K. Leung](http://graphics.cs.cmu.edu/people/efros/research/NPS/efros-iccv99.pdf)
+## Stamenov Sergii
+
+### Depencencies
+ 1. `numpy`
+ 2. `sklearn`
+ 3. `matplotlib`
+
+ ### Usage
+ `python texture_generation.py input_file.jpg window_size output_width output_height`
--- texture_generation.py
+++ texture_generation.py
@@ -1,12 +1,10 @@
-# constants
+import string
 import numpy as np
 import argparse
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import multiprocessing
 from sklearn.feature_extraction import image as img_features
 
-ERR_TRESHOLD = 0.1
 
 class Pixel:
     def __init__(self, x, y):
@@ -36,7 +34,9 @@ def get_unfilled_pixel_neighbours_list(image, window_size):
     return sorted(lst, key=lambda x: x.neighbor_count, reverse=True)
 
 
-def get_candidate_list(template, gauss_mask, patches, sample_shape):
+def get_candidate_list(template, gauss_mask, patches, win_size):
+    threshold = 0.1
+
     mask = template != 0
     weight = gauss_mask * mask
     total_weight = np.sum(weight)
@@ -44,14 +44,15 @@ def get_candidate_list(template, gauss_mask, patches, sample_shape):
     if total_weight == 0:
         total_weight = 1
 
-    SSD = np.sum(((patches - template) ** 2) * (weight), axis=(1, 2)) / float(total_weight)
+    SSD = np.sum(np.multiply((patches - template) ** 2, weight), axis=(1, 2)) / float(total_weight)
+
     min_err = SSD.min()
     candidates = []
     for i, err in enumerate(SSD):
-        if err <= min_err * (1 + ERR_TRESHOLD):
-            pos = np.unravel_index(i, (56, 56))
-            candidate = Pixel(pos[0], pos[1])
+        if err <= min_err * (1 + threshold):
+            candidate = Pixel(0, 0)
             candidate.error = err
+            candidate.value = patches[i, win_size / 2, win_size / 2]
             candidates.append(candidate)
     return candidates
 
@@ -68,75 +69,79 @@ def get_gauss_mask(window_size):
     return gauss
 
 
-# numpy arrays
-def grow_image(sample_image, image, window_size):
+def grow_image(sample_image, image, image_size, window_size):
+    unfilled = image_size - np.count_nonzero(image)
+    max_err_treshold = 0.3
+    iteration = 1
+    prev = 0
+
     half_window = window_size / 2
     g_mask = get_gauss_mask(window_size)
     patches = img_features.extract_patches_2d(sample_image, (window_size, window_size))
 
-    filled_mask = np.zeros(image.shape)
-
-    max_err_treshold = 0.3
-    iteration = 1
-    unfilled = image.size - np.count_nonzero(image)
-    prev = unfilled
     while unfilled:
         flag = False
-        # same points ?
+
         pixel_list = get_unfilled_pixel_neighbours_list(image, window_size)
-        # parallel loop
-        # pool.map(, pixel_list)
+
         for pixel in pixel_list:
             template = image[pixel.x - half_window:pixel.x + half_window + 1,
                        pixel.y - half_window:pixel.y + half_window + 1]
-            candidates = get_candidate_list(template, g_mask, patches, sample_image.shape)
+
+            candidates = get_candidate_list(template, g_mask, patches, window_size)
 
             if len(candidates) == 0:
                 continue
 
             candidate = np.random.choice(candidates)
             if candidate.error <= max_err_treshold:
-                if sample_image[candidate.x + half_window, candidate.y + half_window] == 0:
-                    print 'wut'
-                image[pixel.x, pixel.y] = sample_image[candidate.x + half_window, candidate.y + half_window]
-                filled_mask[pixel.x, pixel.y] = 1
+                image[pixel.x, pixel.y] = candidate.value
                 flag = True
                 unfilled -= 1
-                print 'filled at %d,%d, left %d ' % (pixel.x, pixel.y, unfilled)
+                print 'filled %f' % ((image_size - float(unfilled)) / image_size)
 
-            if (prev - unfilled) > 300:
-                prev = unfilled
+            progress = (image_size - float(unfilled)) / image_size
+            if progress - prev >= 0.1:
                 mpimg.imsave('tmp/progress_%d.png' % iteration, image, cmap=plt.get_cmap('gray'))
                 iteration += 1
+                prev = progress
 
         if not flag:
             max_err_treshold *= 1.1
-            print 'err'
+
     return image
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    window_size = 21
+    parser.add_argument('input_file', type=str, help='sample file path')
+    parser.add_argument('window_size', type=int, help='sliding window size')
+    parser.add_argument('width', type=int, help='generated texture size')
+    parser.add_argument('height', type=int, help='generated texture size')
+
+    args = parser.parse_args()
+
+    window_size = args.window_size
+    filename = args.input_file
     half_w = window_size / 2
 
-    # so far we will work with grayscale
-    img = mpimg.imread('src.gif')
-    # gray_image = 0.2126 * img[:, :, 0] + 0.7152 * img[:, :, 1] + 0.0722 * img[:, :, 2]
-    img = img[:56, :56]
+    size = (args.width, args.height)
+    image_size = (args.width + half_w * 2, args.height + half_w * 2)
 
-    # pad image with zeros
-    # w = img.shape[0] + half_w * 2
-    # h = img.shape[1] + half_w * 2
+    img = mpimg.imread(filename)
 
-    # _sample = np.zeros((w, h))
-    # _sample[half_w:-half_w, half_w:-half_w] = img
+    # rgb case, transform to grayscale
+    if len(img.shape) > 2:
+        gray_image = (0.2126 * img[:, :, 0] + 0.7152 * img[:, :, 1] + 0.0722 * img[:, :, 2]) / 255.0
+        img = gray_image
+    else:
+        img /= 255.0
+
     sample = img
 
     mpimg.imsave('sample.png', sample, cmap=plt.get_cmap('gray'))
 
     # new image we would like to generate
-    image_size = (128, 128)
     target = np.zeros(image_size)
 
     # seed the image
@@ -148,5 +153,5 @@ if __name__ == '__main__':
     center_y = (image_size[1] - seed_size) / 2
 
     target[center_x:center_x + seed_size, center_y:center_y + seed_size] = seed
-    target = grow_image(sample, target, window_size)
-    mpimg.imsave('test.png', target, cmap=plt.get_cmap('gray'))
+    target = grow_image(sample, target, size[0] * size[1], window_size)
+    mpimg.imsave('output.png', target[half_w:-half_w, half_w:-half_w], cmap=plt.get_cmap('gray'))
